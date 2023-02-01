861f0d2cb8db333639f574e931cd428bf7beba68
Marcus Twichel <marc.twichy@gmail.com>
2023-02-01 02:20:36 -0700

    fix: subscirption error

--- api/lib/message_stream.dart
+++ api/lib/message_stream.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:api/message.dart';
 
 final _controller = StreamController<Message>();
-Stream<Message> get messagesStream => _controller.stream.asBroadcastStream();
+final Stream<Message> messagesStream = _controller.stream.asBroadcastStream();
 void addMessage(Message message) {
   _controller.add(message);
 }
