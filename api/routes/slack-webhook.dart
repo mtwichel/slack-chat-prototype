0606576e91a1c058d47575dc992b1f26c6bce57f
Marcus Twichel <marc.twichy@gmail.com>
2023-02-01 02:25:04 -0700

    respond to slack messages

--- api/routes/slack-webhook.dart
+++ api/routes/slack-webhook.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:api/message.dart';
+import 'package:api/message_stream.dart';
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
@@ -11,11 +15,11 @@ Future<Response> onRequest(RequestContext context) async {
       final challenge = requestBody['challenge'] as String;
       return Response.json(body: {'challenge': challenge});
   }
-  // final message = Message(
-  //   content: await context.request.body(),
-  //   senderId: 'slack',
-  //   sentAt: DateTime.now(),
-  // );
-  // addMessage(message);
+  final message = Message(
+    content: jsonEncode(requestBody),
+    senderId: 'slack',
+    sentAt: DateTime.now(),
+  );
+  addMessage(message);
   return Response();
 }
